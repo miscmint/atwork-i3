739e16e1d7775764d726966bc730021f762c08fd
Daniel Simon <dsimon@b-factor.de>
2018-12-06 07:34:47 +0100

    initial release

---
+++ scripts/screenshot.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+import -window root ~/Bilder/Screenshot$(date +%F%H:%M:%S).png"
+
+notify-send 'Screenshot wurde im Ordner Bilder angelegt'
---
+++ scripts/unmount.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+RETURN=$(umount /media/daniel/* 2>&1 >/dev/null) 
+
+if [ -z "$RETURN" ]; then
+	notify-send -t 3000 'Unmount erfolgreich'
+
+else
+	notify-send -t 3000 -u critical 'Unmount nicht möglich, wahrscheinlich wird ein Gerät noch verwendet oder alle Geräte sind schon entfernt.'
+fi
+
+exit
